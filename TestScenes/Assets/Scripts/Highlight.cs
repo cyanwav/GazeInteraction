3ea836daf370ee434414084f15c85f76f1cf094f
WhirlpoolSeaweed <yorktown1781@163.com>
2024-01-26 00:12:10 +0100

    Finish MainCamera version of Highlighting targets
    Usage: attach highligh.cs to targets and refer to main camera

--- TestScenes/Assets/Scripts/Highlight.cs
+++ TestScenes/Assets/Scripts/Highlight.cs
@@ -4,29 +4,24 @@ using System.IO;
 public class Highlight : MonoBehaviour
 {
     private bool isHighlighted;
-    private int highlightedObjNum = 0;
-    private float startTime;
-    private float endTime;
-    private List<string> csvData;
-    private int TotalObjNum = 4;
-    public bool isTarget = false;
-    [SerializeField]
-    private List<Renderer> renderers;
-    [SerializeField]
-    private Color color = Color.white;
-    private List<Material> materials;
-    void Start()
-    {
-        csvData = new List<string>();
-    }
+    // public bool isTarget = false;
+    public GameObject cameraSource;
+
+
+    // [SerializeField]
+    // private List<Renderer> renderers;
+    // [SerializeField]
+    // private Color color = Color.white;
+    // private List<Material> materials;
+
     private void Awake()
     {
-        materials = new List<Material>();
-        foreach (var renderer in renderers)
-        {
-            materials.AddRange(new List<Material>(renderer.materials));
-        }
-        color.a = 0.12f;
+        // materials = new List<Material>();
+        // foreach (var renderer in renderers)
+        // {
+        //     materials.AddRange(new List<Material>(renderer.materials));
+        // }
+        // color.a = 0.12f;
         if (isHighlighted)
         {
             ToggleHighlight(true);
@@ -34,77 +29,55 @@ public class Highlight : MonoBehaviour
     }
     public void ToggleHighlight(bool val)
     {
+        HighlightOnRaycast rayScript = cameraSource.GetComponent<HighlightOnRaycast>();
         // if a target is highlighted, it will not be highlighted again. 
         if (val && !isHighlighted)
         {
             isHighlighted = true;
-            if (isTarget)
+            // if (isTarget)
+            // {
+            float foundTime = Time.time;
+            string dataEntry = $"{foundTime}";
+            rayScript.csvData.Add(dataEntry);
+            rayScript.highlightedObjNum++;
+            if (!rayScript.isStarted)
             {
-                float foundTime = Time.time;
-                string dataEntry = $"{foundTime}";
-                csvData.Add(dataEntry);
-                highlightedObjNum++;
-                if (highlightedObjNum == 0)
-                {
-                    startTime = foundTime;
-                    Debug.Log("Start Recording: " + startTime);
-                }
-                else Debug.Log("Highlighted Targets: " + (highlightedObjNum - 1));
+                rayScript.isStarted = true;
+                rayScript.startTime = foundTime;
+                Debug.Log("Start Recording: " + rayScript.startTime);
             }
-            foreach (var material in materials)
+            else Debug.Log("Highlighted Targets: " + (rayScript.highlightedObjNum - 1));
+            // }
+            // foreach (var material in materials)
+            // {
+            //     color.a = 0.12f;
+            //     material.EnableKeyword("_EMISSION");
+            //     material.SetColor("_EmissionColor", color);
+            // }
+            if (rayScript.highlightedObjNum == rayScript.TotalObjNum)
             {
-                color.a = 0.12f;
-                material.EnableKeyword("_EMISSION");
-                material.SetColor("_EmissionColor", color);
-            }
-            if (highlightedObjNum == TotalObjNum)
-            {
-                endTime = Time.time;
-                string dataEntry = $"{endTime}";
-                csvData.Add(dataEntry);
-                Debug.Log("End Recording: " + endTime);
+                rayScript.endTime = Time.time;
+                dataEntry = $"{rayScript.endTime}";
+                rayScript.csvData.Add(dataEntry);
+                Debug.Log("End Recording: " + rayScript.endTime);
                 //record duration
-                dataEntry = $"{endTime - startTime}";
-                csvData.Add(dataEntry);
-                Debug.Log("Duration: " + (endTime - startTime));
-                highlightedObjNum = 0;
+                dataEntry = $"{rayScript.endTime - rayScript.startTime}";
+                rayScript.csvData.Add(dataEntry);
+                Debug.Log("Duration: " + (rayScript.endTime - rayScript.startTime));
+                rayScript.highlightedObjNum = 0;
             }
         }
-        else
-        {
-            if (!isTarget)
-            {
-                isHighlighted = false;
-                foreach (var material in materials)
-                {
-                    material.DisableKeyword("_EMISSION");
-                }
-            }
-        }
-    }
-    private void WriteCsvToFile(string filePath, List<string> data)
-    {
-        // Ensure the directory exists
-        string directory = Path.GetDirectoryName(filePath);
-        Debug.Log("Directory: " + directory);
-        if (!Directory.Exists(directory))
-        {
-            Directory.CreateDirectory(directory);
-        }
-        // Write data to CSV file
-        File.WriteAllLines(filePath, data);
-        Debug.Log("CSV file written: " + filePath);
-    }
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            // Write  face and gaze CSV data to file when 'Esc' key is pressed
-            string fileBase = "test";
-            string outFilePathGaze = Path.Combine("", fileBase + "_Gaze.csv");
-            WriteCsvToFile(outFilePathGaze, csvData);
-            Debug.Log("CSV Gaze file written: " + outFilePathGaze);
-        }
+        // else
+        // {
+        //     if (!isTarget)
+        //     {
+        //         isHighlighted = false;
+        //         // foreach (var material in materials)
+        //         // {
+        //         //     material.DisableKeyword("_EMISSION");
+        //         // }
+        //     }
+        // }
     }
 }
 
--- TestScenes/Assets/Scripts/HighlightOnRaycast.cs
+++ TestScenes/Assets/Scripts/HighlightOnRaycast.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class HighlightOnRaycast : MonoBehaviour
 {
-    private GameObject lastMainParent;
+    // private GameObject lastMainParent;
     public GameObject sourceObject;
-    GameObject FindPrefabParent(GameObject child)
+    private GameObject lastHitObject;
+    public int highlightedObjNum;
+    public float startTime;
+    public float endTime;
+    public List<string> csvData;
+    public int TotalObjNum = 4;
+    public bool isStarted = false;
+    // GameObject FindPrefabParent(GameObject child)
+    // {
+    //     Transform currentParent = child.transform;
+    //     // Keep traversing up the hierarchy until the parent is null
+    //     while (currentParent.parent != null)
+    //     {
+    //         currentParent = currentParent.parent;
+    //         // Check if the current parent has the specified tag
+    //         if (currentParent.CompareTag("SimObjPhysics"))
+    //         {
+    //             // Stop the search and return the current parent
+    //             return currentParent.gameObject;
+    //         }
+    //     }
+    //     // No prefab parent found
+    //     return null;
+    // }
+    void Start()
     {
-        Transform currentParent = child.transform;
-        // Keep traversing up the hierarchy until the parent is null
-        while (currentParent.parent != null)
-        {
-            currentParent = currentParent.parent;
-            // Check if the current parent has the specified tag
-            if (currentParent.CompareTag("SimObjPhysics"))
-            {
-                // Stop the search and return the current parent
-                return currentParent.gameObject;
-            }
-        }
-        // No prefab parent found
-        return null;
+        csvData = new List<string>();
     }
 
     void Update()
@@ -38,54 +50,104 @@ public class HighlightOnRaycast : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             // If the ray hit an object
-            Debug.Log(hit.collider.gameObject.name);
-            GameObject mainParent = FindPrefabParent(hit.collider.gameObject);
-            if (mainParent == null)
+            // Debug.Log(hit.collider.gameObject.name);
+            // GameObject mainParent = FindPrefabParent(hit.collider.gameObject);
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+            // if (mainParent == null)
+            if (renderer == null)
             {
                 // Debug.Log("Main parent not found");
             }
             else
             {
-                Debug.Log("Found parent: " + mainParent.name);
-                Highlight highlightComponent = mainParent.GetComponent<Highlight>();
+                // Debug.Log("Found parent: " + mainParent.name);
+                // Highlight highlightComponent = mainParent.GetComponent<Highlight>();
+                Highlight highlightComponent = hit.collider.gameObject.GetComponent<Highlight>();
                 if (highlightComponent != null)
                 {
                     // If the last hit object is not the current hit object
-                    if (lastMainParent != null && lastMainParent != mainParent)
+                    // if (lastMainParent != null && lastMainParent != mainParent)
+                    if (lastHitObject != null && lastHitObject != hit.collider.gameObject)
                     {
                         // Return the color of the last hit object to white
-                        Highlight lastHighlightComponent = lastMainParent.GetComponent<Highlight>();
-                        if (lastHighlightComponent != null)
+                        // Highlight lastHighlightComponent = lastMainParent.GetComponent<Highlight>();
+                        Renderer lastRenderer = lastHitObject.GetComponent<Renderer>();
+                        if (lastRenderer != null)
                         {
-                            Debug.Log("De-highlighting object: " + lastMainParent.name);
-                            lastHighlightComponent.ToggleHighlight(false);
+                            lastRenderer.material.color = Color.white;
+                            Highlight lastHighlightComponent = lastHitObject.GetComponent<Highlight>();
+                            if (lastHighlightComponent != null)
+                            {
+                                // Debug.Log("De-highlighting object: " + lastMainParent.name);
+                                // Debug.Log("De-highlighting object: " + lastHitObject.name);
+                                lastHighlightComponent.ToggleHighlight(false);
+                            }
                         }
                     }
                     // Change the color of the hit object
+                    renderer.material.color = Color.black;
                     highlightComponent.ToggleHighlight(true);
                     // Update the last hit object
-                    lastMainParent = mainParent;
+                    // lastMainParent = mainParent;
+                    lastHitObject = hit.collider.gameObject;
 
                 }
-                else
-                {
-                    Debug.Log("Highlight component not found on " + mainParent.name);
-                }
+                // else
+                // {
+                //     Debug.Log("Highlight component not found on " + mainParent.name);
+                // }
             }
         }
         else
         {
             // If the ray does not hit any object, de-highlight the last hit object
-            if (lastMainParent != null)
+            // if (lastMainParent != null)
+            // {
+            //     Highlight lastHighlightComponent = lastMainParent.GetComponent<Highlight>();
+            //     if (lastHighlightComponent != null)
+            //     {
+            //         Debug.Log("De-highlighting object: " + lastMainParent.name);
+            //         lastHighlightComponent.ToggleHighlight(false);
+            //     }
+            //     lastMainParent = null;
+            // }
+            if (lastHitObject != null)
             {
-                Highlight lastHighlightComponent = lastMainParent.GetComponent<Highlight>();
-                if (lastHighlightComponent != null)
+                Renderer lastRenderer = lastHitObject.GetComponent<Renderer>();
+                if (lastRenderer != null)
                 {
-                    Debug.Log("De-highlighting object: " + lastMainParent.name);
-                    lastHighlightComponent.ToggleHighlight(false);
+                    lastRenderer.material.color = Color.white;
+                    Highlight lastHighlightComponent = lastHitObject.GetComponent<Highlight>();
+                    if (lastHighlightComponent != null)
+                    {
+                        // Debug.Log("De-highlighting object: " + lastHitObject.name);
+                        lastHighlightComponent.ToggleHighlight(false);
+                    }
+                    lastHitObject = null;
                 }
-                lastMainParent = null;
             }
         }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            // Write  face and gaze CSV data to file when 'Esc' key is pressed
+            string fileBase = "test";
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string outFilePathGaze = Path.Combine(currentDirectory, fileBase + "_Gaze.csv");
+            WriteCsvToFile(outFilePathGaze, csvData);
+            Debug.Log("CSV Gaze file written: " + outFilePathGaze);
+        }
+    }
+    private void WriteCsvToFile(string filePath, List<string> data)
+    {
+        // Ensure the directory exists
+        string directory = Path.GetDirectoryName(filePath);
+        Debug.Log("Directory: " + directory);
+        // if (!Directory.Exists(directory))
+        // {
+        //     Directory.CreateDirectory(directory);
+        // }
+        // Write data to CSV file
+        File.WriteAllLines(filePath, data);
+        Debug.Log("CSV file written: " + filePath);
     }
 }
